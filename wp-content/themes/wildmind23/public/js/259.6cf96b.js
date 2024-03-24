1bb9b9c056df3c120df81dcbcaede35fcb57984a
Keith <keith@vool.ie>
2024-03-24 14:43:08 +0000

    Add 2023 archive

---
+++ wp-content/themes/wildmind23/public/js/259.6cf96b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_roots_bud_sage_sage=self.webpackChunk_roots_bud_sage_sage||[]).push([[259],{"../node_modules/@roots/sage/lib/client/dom-ready.js":(e,s)=>{s.Z=e=>{window.requestAnimationFrame((async function s(){document.body?await e():window.requestAnimationFrame(s)}))}}}]);
---
+++ wp-content/themes/wildmind23/public/js/app.ff7fef.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_roots_bud_sage_sage=self.webpackChunk_roots_bud_sage_sage||[]).push([[143],{"./styles/app.css":()=>{},"./scripts/app.js":(s,e,a)=>{(0,a("../node_modules/@roots/sage/lib/client/dom-ready.js").Z)((async()=>{}))}},s=>{var e=e=>s(s.s=e);s.O(0,[259],(()=>(e("./scripts/app.js"),e("./styles/app.css")))),s.O()}]);
